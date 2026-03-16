039724cfa78bfdd0a0d3ce3efd1d07af817cbc0f
Dino <keetawasan.life@gmail.com>
2026-03-16 16:55:18 +0700

    Change to finish them all!!

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'portfolio.db'));
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'portfolio.db');
+const db = new Database(dbPath);
 
 // Initialize schema
 db.exec(`
